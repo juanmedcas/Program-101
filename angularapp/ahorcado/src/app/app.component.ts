118ac8bceb129f93aea70d1b9f9f3631a42fb095
juanmedcas <juanmedina10013@gmail.com>
2022-02-16 18:46:52 -0500

    Inicio Proyecto VideoJuego

--- angularapp/ahorcado/src/app/app.component.ts
+++ angularapp/ahorcado/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ahorcado';
+
+  letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+  constructor() {
+    console.log(`Se acabe de crear el App Component`);
+  }
 }
